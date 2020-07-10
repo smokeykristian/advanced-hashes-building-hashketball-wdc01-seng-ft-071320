2a16db05e6b98c7394285f00099af85fcb785f33
Kristian Portillo <portillk@gmail.com>
2020-07-10 12:29:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,17 @@
 # Write your code here!
+def game_hash
+  hash ={ 
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [{
+        :player_name
+        
+      }]
+      
+       }
+    ,
+    :away => {
+      
+    }
+    }
